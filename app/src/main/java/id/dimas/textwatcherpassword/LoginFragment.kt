99fe6bf840d9666766f1c9f9ccaa54d213bd6590
Tri Dimas Cipto Satrio Wibowo <dimassatrio102@gmail.com>
2022-12-01 18:34:09 +0700

    textwatcher password

---
+++ app/src/main/java/id/dimas/textwatcherpassword/LoginFragment.kt
@@ -0,0 +1,43 @@
+package id.dimas.textwatcherpassword
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import id.dimas.textwatcherpassword.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        moveRegist()
+    }
+
+    private fun moveRegist() {
+        binding.tvRegister.setOnClickListener {
+            it.findNavController()
+                .navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
+        }
+    }
+
+}
